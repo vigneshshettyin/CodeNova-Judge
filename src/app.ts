6323272f0eedbb27faa590aa58a3d28214af1b49
Vignesh Shetty <vigneshshetty.in@gmail.com>
2025-03-02 10:51:48 +0530

    feat : domain for cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express from "express";
-// cors
 import cors from "cors";
 import bodyParser from "body-parser";
 import judgeRoutes from "./api/routes/judge.routes";
 import "./queue/rabbitmq";
 
 const app = express();
-app.use(cors());
+
+const CLIENT_DOMAIN = process.env.CLIENT_DOMAIN || "http://localhost:3000";
+const corsOptions = {
+  origin: CLIENT_DOMAIN,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 app.use("/api", judgeRoutes);
 
